18990704a33491fb370d94b662f1b7a4406842d0
George <george@mycontent.gr>
2017-02-14 18:48:40 +0100

    Global prosodic profile

--- app/statistics/prosody/GlobalProsodicProfileWidget.cpp
+++ app/statistics/prosody/GlobalProsodicProfileWidget.cpp
@@ -2,12 +2,17 @@
 #include <QApplication>
 #include <QProgressBar>
 #include <QStandardItemModel>
+#include <QFile>
+#include <QFileInfo>
 
 #include "GlobalProsodicProfileWidget.h"
 #include "ui_GlobalProsodicProfileWidget.h"
 
+#include "pncore/corpus/Corpus.h"
 #include "pncore/datastore/CorpusRepository.h"
 #include "pncore/datastore/MetadataDatastore.h"
+#include "pncore/datastore/AnnotationDatastore.h"
+#include "pncore/annotation/AnnotationDataTable.h"
 using namespace Praaline::Core;
 
 #include "pngui/widgets/GridViewWidget.h"
@@ -50,7 +55,60 @@ GlobalProsodicProfileWidget::~GlobalProsodicProfileWidget()
 
 void GlobalProsodicProfileWidget::analyse()
 {
+    if (!d->repository) return;
+    QString corpusID = ui->comboBoxCorpus->currentText();
+    QPointer<Corpus> corpus = d->repository->metadata()->getCorpus(corpusID);
+    if (!corpus) return;
 
+    QStandardItemModel *model = new QStandardItemModel(this);
+    bool createHeaders = true;
+    foreach (QPointer<CorpusCommunication> com, corpus->communications()) {
+        if (!com) continue;
+        foreach (QPointer<CorpusRecording> rec, com->recordings()) {
+            if (!rec) continue;
+            foreach (QPointer<CorpusAnnotation> annot, com->annotations()) {
+                if (!annot) continue;
+                QStringList speakerIDs = d->repository->annotations()->getSpeakersInAnnotation(annot->ID());
+                foreach (QString speakerID, speakerIDs) {
+                    QFileInfo info(rec->filePath());
+                    QString prosoPath = info.absoluteDir().absolutePath() + "/prosogram/";
+                    QString filenameGlobalsheet = QString("%1_%2_globalsheet.txt").arg(rec->ID()).arg(speakerID);
+                    AnnotationDataTable table;
+                    if (!(table.readFromFile(prosoPath + filenameGlobalsheet))) {
+                        qDebug() << "Error reading: " << filenameGlobalsheet;
+                        continue;
+                    }
+                    // If this is the first table to process, create headers
+                    if (createHeaders) {
+                        QStringList headers;
+                        headers << "CommunicationID" << "RecordingID" << "SpeakerID";
+                        headers << table.getFieldNames();
+                        model->setHorizontalHeaderLabels(headers);
+                        createHeaders = false;
+                    }
+                    // Process data from table
+                    for (int i = 0; i < table.getRowCount(); i++) {
+                        QList<QStandardItem *> items;
+                        items.append(new QStandardItem(com->ID()));
+                        items.append(new QStandardItem(rec->ID()));
+                        items.append(new QStandardItem(speakerID));
+                        foreach (QString attributeName, table.getFieldNames()) {
+                            QVariant value = table.getData(i, attributeName);
+                            QStandardItem *item = new QStandardItem();
+                            item->setData(value, Qt::DisplayRole);
+                            items.append(item);
+                        }
+                        model->appendRow(items);
+                    }
+                } // speaker
+            } // annotation
+        } // recording
+    } // commmunication
+
+    // Update table
+    d->gridviewResults->tableView()->setModel(model);
+    if (d->modelResults) { d->modelResults->clear(); delete d->modelResults; }
+    d->modelResults = model;
 }
 
 } // namespace StatisticsPluginProsody
